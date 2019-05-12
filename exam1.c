acdab74c666fba33e8277566dfe24ea1f8909819
storm18 <ngoducnd52961@gmail.com>
2019-05-12 23:56:01 +0700

    add doc c and linux

--- exam1.c
+++ exam1.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
+#include<stdlib.h>
+int a =10;
+int *b = &a;
 int main()
 {
-printf("start \n C professional");
+printf("start \n C professional\n");
+printf("%d - %d\n",b,sizeof(b));
+b = malloc(3 * sizeof(char));
+printf("%d - %d\n",b,sizeof(b));
+b = malloc(3 * sizeof(char));
+printf("%d - %d\n",b,sizeof(b));
+char *c = malloc(3 * sizeof(char));
+char* a;
+a = malloc(200*sizeof(char));
 return 0;
 }
